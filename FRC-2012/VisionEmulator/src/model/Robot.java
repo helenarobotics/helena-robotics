956e00524e46356c72afa1b29583663d5a369d3c
Nate Williams <nate@yogotech.com>
2012-02-09 09:26:15 -0700

    - Correct transposed the X/Y axis

--- FRC-2012/VisionEmulator/src/model/Robot.java
+++ FRC-2012/VisionEmulator/src/model/Robot.java
@@ -5,14 +5,14 @@ import java.util.Observable;
 // This class keeps track of the state of the camera on the robot
 public class Robot extends Observable {
     // The field length in inches
-    private static final int MAX_X_OFFSET = 57 * 12;
+    private static final int MAX_Y_OFFSET = 57 * 12;
 
     // The fender limits how close we can get
-    private static final int MIN_X_OFFSET = 39;
+    private static final int MIN_Y_OFFSET = 39;
 
     // The field width is half of 27'
-    private static final int MAX_Y_OFFSET = 27 * 12 / 2;
-    private static final int MIN_Y_OFFSET = -27 * 12 / 2;
+    private static final int MAX_X_OFFSET = 27 * 12 / 2;
+    private static final int MIN_X_OFFSET = -27 * 12 / 2;
 
     // XXX - Arbitrarily chose to move 1 foot at a time.
     private static final int MOVE_AMT = 12;
@@ -22,11 +22,11 @@ public class Robot extends Observable {
     // xOffset = 36 would be 3 feet to the right of the basket
     //
     // yOffset = 0 would be in the center of the court
-    // yOffset = 36 would be 3 feet to the right of the basket
+    // yOffset = 36 would be 3 feet behind (in front of) the basket
     //
     // rotation = 0 would face the basket directly (assuming yOffset = 0)
     // rotation = 45 would rotate the robot 45 degrees clockwise, so the
-    //   basket would be to the left of the robot (assuming yOffset = 0)
+    //   basket would be to the left of the robot (assuming xOffset = 0)
     private int xOffset;
     private int yOffset;
     private int rotation;
@@ -51,8 +51,8 @@ public class Robot extends Observable {
     // Reset the camera back to 'zero' position
     public void resetPosition() {
         // Center court, 20 feet away, facing the basket
-        xOffset = 20 * 12;
-        yOffset = 0;
+        xOffset = 0;
+        yOffset = 20 * 12;
         rotation = 0;
         setChanged();
         notifyObservers(rotation);
@@ -60,31 +60,18 @@ public class Robot extends Observable {
 
     // Move the robot!
     public void moveIn() {
-        if (xOffset > 0) {
-            xOffset -= MOVE_AMT;
+        if (yOffset > 0) {
+            yOffset -= MOVE_AMT;
             // Fender limits us getting any closer than 39"
-            if (xOffset < MIN_X_OFFSET)
-                xOffset = MIN_X_OFFSET;
+            if (yOffset < MIN_Y_OFFSET)
+                yOffset = MIN_Y_OFFSET;
             setChanged();
-            notifyObservers(xOffset);
+            notifyObservers(yOffset);
         }
     }
 
     // Move the robot!
     public void moveOut() {
-        if (xOffset < MAX_X_OFFSET) {
-            xOffset += MOVE_AMT;
-            // Since we can move in MOVE_AMT increments, limit us to
-            // being inside the field.
-            if (xOffset > MAX_X_OFFSET)
-                xOffset = MAX_X_OFFSET;
-            setChanged();
-            notifyObservers(xOffset);
-        }
-    }
-
-    // Move the robot!
-    public void strafeRight() {
         if (yOffset < MAX_Y_OFFSET) {
             yOffset += MOVE_AMT;
             // Since we can move in MOVE_AMT increments, limit us to
@@ -96,15 +83,28 @@ public class Robot extends Observable {
         }
     }
 
-    public void strafeLeft() {
-        if (yOffset > MIN_Y_OFFSET) {
-            yOffset -= MOVE_AMT;
+    // Move the robot!
+    public void strafeRight() {
+        if (xOffset < MAX_X_OFFSET) {
+            xOffset += MOVE_AMT;
             // Since we can move in MOVE_AMT increments, limit us to
             // being inside the field.
-            if (yOffset < MIN_Y_OFFSET)
-                yOffset = MIN_Y_OFFSET;
+            if (xOffset > MAX_X_OFFSET)
+                xOffset = MAX_X_OFFSET;
             setChanged();
-            notifyObservers(yOffset);
+            notifyObservers(xOffset);
+        }
+    }
+
+    public void strafeLeft() {
+        if (xOffset > MIN_X_OFFSET) {
+            xOffset -= MOVE_AMT;
+            // Since we can move in MOVE_AMT increments, limit us to
+            // being inside the field.
+            if (xOffset < MIN_X_OFFSET)
+                xOffset = MIN_X_OFFSET;
+            setChanged();
+            notifyObservers(xOffset);
         }
     }
 
