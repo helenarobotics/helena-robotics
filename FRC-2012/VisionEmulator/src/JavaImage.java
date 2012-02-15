62f918eef50bb3bd464bd38520007eb118454c90
james arnold <jamesfarnold@gmail.com>
2012-02-15 10:14:52 -0700

    Work in progress.  Updated and experimentally tuned edge detectors, now converts to grayscale (based on color match, a feature not yet fully baked), and outputs a number of image files for debugging purposes.  The "sum" function has a known bug that I will fix soon.  NB some form of the edge files created here will be input to the HoughTransform to generate hoop edge lines as pairs of points; we will then find intersections of those lines to identify corners.  Will see how well this works...

--- FRC-2012/VisionEmulator/src/JavaImage.java
+++ FRC-2012/VisionEmulator/src/JavaImage.java
@@ -7,8 +7,15 @@ import java.awt.image.ColorModel;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 import java.awt.image.WritableRaster;
+import java.awt.image.Raster;
+import java.awt.Graphics;
 
 import javax.imageio.ImageIO;
+
+// following additions for HoughTransform section.  Unclear that we need the complete awt imported...
+import java.awt.*; 
+import java.util.Vector; 
+
 
 public class JavaImage {
     public static void main(String args[]) {
@@ -19,6 +26,7 @@ public class JavaImage {
         }
 
         // Validate *ALL* the arguments before processing any of them.
+
         for (int fileNum = 0; fileNum < args.length; fileNum++) {
             File f = new File(args[fileNum]);
             if (!f.exists()) {
@@ -29,18 +37,31 @@ public class JavaImage {
         }
 
         // Convolve to find the edges
-        Kernel sobelXKern = new Kernel(3, 3, new float[] {
-                                           -1.0f, 0.0f, 1.0f,
-                                           -2.0f, 0.0f, 2.0f,
-                                           -1.0f, 0.0f, 1.0f
-                                       });
-        Kernel sobelYKern = new Kernel(3, 3, new float[] {
-                                           1.0f, 2.0f, 1.0f,
-                                           0.0f, 0.0f, 0.0f,
-                                           -1.0f, -2.0f, -1.0f
-                                       });
-        BufferedImageOp xop = new ConvolveOp(sobelXKern);
-        BufferedImageOp yop = new ConvolveOp(sobelYKern);
+	Kernel sobelXKernR = new Kernel(3, 3, new float[] {
+		-1.0f, 0.0f, 0.0f, 
+		-2.0f, 0.0f, 2.0f, 
+		-1.0f, 0.0f, 1.0f
+	    });
+	Kernel sobelXKernL = new Kernel(3, 3, new float[] {
+		0.0f, 0.0f, -1.0f, 
+		2.0f, 0.0f, -2.0f, 
+		1.0f, 0.0f, -1.0f
+	    });
+        Kernel sobelYKernD = new Kernel(3, 3, new float[] {
+		-1.0f, -2.0f, -1.0f,
+		0.0f, 0.0f, 0.0f, 
+		1.0f, 2.0f, 1.0f, 
+	    });
+        Kernel sobelYKernU = new Kernel(3, 3, new float[] {
+		1.0f, 2.0f, 1.0f, 
+		0.0f, 0.0f, 0.0f, 
+		-1.0f, -2.0f, -1.0f,
+	    });
+
+        BufferedImageOp xopR = new ConvolveOp(sobelXKernR);
+        BufferedImageOp xopL = new ConvolveOp(sobelXKernL);
+        BufferedImageOp yopU = new ConvolveOp(sobelYKernU);
+        BufferedImageOp yopD = new ConvolveOp(sobelYKernD);
 
         long totalTime = 0;
         
@@ -48,44 +69,135 @@ public class JavaImage {
         boolean success = true;
         for (int fileNum = 0; success && fileNum < args.length; fileNum++) {
             String imageFile = args[fileNum];
-//            for (int r = 0; r < 100; r++) {
+	    System.out.println("processing file '" + imageFile + "'");
             long startTime = System.currentTimeMillis();
             try {
-                BufferedImage img = ImageIO.read(new File(imageFile));
+                BufferedImage colorImg = ImageIO.read(new File(imageFile));
 
-                // Create a new image to store the convolved data.
+		// convert to probability-of-backboard function (simplified for now):
+
+		BufferedImage img = detectRed(colorImg);
+
 //                long startTime = System.currentTimeMillis();
-                BufferedImage xImg = deepCopy(img);
-                BufferedImage edgeImg = deepCopy(img);
-                xop.filter(img, xImg);
-                yop.filter(xImg, edgeImg);
+                // Create a new image to store the convolved data.
+                BufferedImage xImgL = deepCopy(img);
+                BufferedImage xImgR = deepCopy(img);
+                BufferedImage yImgD = deepCopy(img);
+                BufferedImage yImgU = deepCopy(img);
+
+		xopL.filter(img, xImgL);
+		xopR.filter(img, xImgR);
+		yopD.filter(img, yImgD);
+		yopU.filter(img, yImgU);
+
                 totalTime += (System.currentTimeMillis() - startTime);
 
-                // Write out new image!
-                String edgeFile = "Edge_" + fileNum + ".jpg";
-                ImageIO.write(edgeImg, "jpg", new File(edgeFile));
-                if (true)
-                continue;
+                // Write out new image set
 
-                int pixelData[][] = new int[img.getHeight() * img.getWidth()][3];
+                ImageIO.write(yImgU, "jpg", new File("yEdgeU" + fileNum));
+                ImageIO.write(yImgD, "jpg", new File("yEdgeD" + fileNum));
+                ImageIO.write(xImgL, "jpg", new File("xEdgeL" + fileNum));
+                ImageIO.write(xImgR, "jpg", new File("xEdgeR" + fileNum));
 
-                int counter = 0;
-                for (int x = 0; x < img.getWidth(); x++){
-                    for (int y = 0; y < img.getHeight(); y++){
-                        int rgb[] = getPixelData(img, x, y);
-                        for (int k = 0; k < rgb.length; k++)
-                            pixelData[counter][k] = rgb[k];
+		BufferedImage iout = addGrayImages(xImgR, yImgD);
 
-                        counter++;
-                    }
-                }
+		ImageIO.write(iout, "jpg", new File("Sum_" + fileNum));
+
+		/*
+		BufferedImage grayscale_image = new BufferedImage(img.getWidth(), img.getHeight(), 
+							BufferedImage.TYPE_BYTE_GRAY);  
+		Graphics g = grayscale_image.getGraphics();  
+		g.drawImage(img, 0, 0, null);  
+		g.dispose();
+		String gFile = "Grayscale_" + fileNum;
+                ImageIO.write(grayscale_image, "jpg", new File(gFile));
+		*/
+
+		ImageIO.write(img, "jpg", new File("Grayscale_" + fileNum));
+
+	    
             } catch (IOException e) {
                 e.printStackTrace();
                 success = false;
             }
-//            }
         }
         System.out.println("Total time = '" + totalTime / 100 + "'");
+    }
+
+
+    private static BufferedImage detectRed(BufferedImage img) {
+
+	 BufferedImage red = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+	 WritableRaster raster = red.getRaster();
+
+	 for (int x = 0; x < img.getWidth(); x++){
+	     for (int y = 0; y < img.getHeight(); y++){
+		 int rgb[] = getPixelData(img, x, y);
+		 int pixelValue = rgb[0] - Math.abs(rgb[1] - rgb[2]) - rgb[1]/2 - rgb[2]/2;
+		 if (pixelValue > 255) pixelValue = 255;
+		 else if (pixelValue < 0) pixelValue = 0;
+		 raster.setSample(x, y, 0, pixelValue);
+	     }
+	 }
+	 return red;
+    }
+
+
+
+    private static BufferedImage addGrayImages(BufferedImage img1, BufferedImage img2, BufferedImage img3, BufferedImage img4) {
+
+	 BufferedImage out = new BufferedImage(img1.getWidth(), img1.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+	 WritableRaster o = out.getRaster();
+	 Raster r1 = img1.getData();
+	 Raster r2 = img2.getData();
+	 Raster r3 = img3.getData();
+	 Raster r4 = img4.getData();
+
+	 int [] buffer = new int [1];
+	 int [] pixel1 = new int [1];
+	 int [] pixel2 = new int [1];
+	 int [] pixel3 = new int [1];
+	 int [] pixel4 = new int [1];
+
+	 for (int x = 0; x < out.getWidth(); x++){
+	     for (int y = 0; y < out.getHeight(); y++){
+		 pixel1 = r1.getPixel(x, y, buffer);
+		 pixel2 = r2.getPixel(x, y, buffer);
+		 pixel3 = r3.getPixel(x, y, buffer);
+		 pixel4 = r4.getPixel(x, y, buffer);
+		 int sum = pixel1[0] + pixel2[0] + pixel3[0] + pixel4[0];
+		 if (sum > 255) sum = 255;
+		 o.setSample(x, y, 0, sum);
+	     }
+	 }
+	 return out;
+    }
+
+
+    private static BufferedImage addGrayImages(BufferedImage img1, BufferedImage img2) {
+
+	 BufferedImage out = new BufferedImage(img1.getWidth(), img1.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+	 WritableRaster o = out.getRaster();
+	 Raster r1 = img1.getData();
+	 Raster r2 = img2.getData();
+
+	 int [] buffer = new int [1];
+	 int [] pixel1 = new int [1];
+	 int [] pixel2 = new int [1];
+
+	 for (int x = 0; x < out.getWidth(); x++){
+	     for (int y = 0; y < out.getHeight(); y++){
+		 pixel1 = r1.getPixel(x, y, buffer);
+		 pixel2 = r2.getPixel(x, y, buffer);
+		 int sum = pixel1[0] + pixel2[0];
+		 if (sum > 255) sum = 255;
+		 o.setSample(x, y, 0, sum);
+	     }
+	 }
+	 return out;
     }
 
     private static int[] getPixelData(BufferedImage img, int x, int y) {
