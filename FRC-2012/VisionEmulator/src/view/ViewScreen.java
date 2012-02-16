c54dd503e6444c9d47049081139cb1d939d7dc08
Nate Williams <nate@yogotech.com>
2012-02-15 23:33:43 -0700

    - Temporarily disable the Hoops and use the 'DistortHoop' class

--- FRC-2012/VisionEmulator/src/view/ViewScreen.java
+++ FRC-2012/VisionEmulator/src/view/ViewScreen.java
@@ -19,41 +19,39 @@ public class ViewScreen extends JPanel implements Observer {
     private Camera camera;
     private Robot robot;
 
-    // XXX - Doesn't belong here, but for now this works
-    private static final double CAMERA_HEIGHT = 20.0 ;
-
-    // The hoop display objects.
-    private Hoop hoops[];
+    // The hoop display objects
+    private DistortHoop hoop;
+//    private Hoop hoops[];
 
     public ViewScreen(Camera _camera, Robot _robot) {
         // Turn on double-buffering
         super(true);
-//        setPreferredSize(new Dimension(1024, 768));
         setPreferredSize(new Dimension(800, 600));
-//        setPreferredSize(new Dimension(640, 480));
         setBackground(Color.BLACK);
         setForeground(Color.WHITE);
-
-        // We'll be drawing images onto this, so no layout!
-        setLayout(null);
-
-        // Create the hoops
-        hoops = new Hoop[4];
-
-        // Bottom hoop
-        hoops[0] = new Hoop(0, 35.0, 28.0 - CAMERA_HEIGHT);
-
-        // Middle two hoops
-        hoops[1] = new Hoop(-27.375, 1.5, 61.0 - CAMERA_HEIGHT);
-        hoops[2] = new Hoop(+27.375, 1.5, 61.0 - CAMERA_HEIGHT);
-
-        // Top hoop
-        hoops[3] = new Hoop(0, -35.0, 98.0 - CAMERA_HEIGHT);
 
         // Keep track of the objects we're monitoring.
         camera = _camera;
         robot = _robot;
 
+        // We'll be drawing images onto this, so no layout!
+        setLayout(null);
+
+        hoop = new DistortHoop(0, robot.getHeight());
+/*
+        // Create the hoops
+        hoops = new Hoop[4];
+
+        // Bottom hoop
+        hoops[0] = new Hoop(0, 35.0, 28.0 - robot.getHeight());
+
+        // Middle two hoops
+        hoops[1] = new Hoop(-27.375, 1.5, 61.0 - robot.getHeight());
+        hoops[2] = new Hoop(+27.375, 1.5, 61.0 - robot.getHeight());
+
+        // Top hoop
+        hoops[3] = new Hoop(0, -35.0, 98.0 - robot.getHeight());
+*/
         // Subscribe to changes in the model
         camera.addObserver(this);
         robot.addObserver(this);
@@ -73,9 +71,10 @@ public class ViewScreen extends JPanel implements Observer {
             // Make the center of the screen 0, 0
             g.translate(getWidth() / 2, getHeight() / 2);
 
+            hoop.paint(g, getWidth(), getHeight(), robot, camera);
             // Draw the hoops
-            for (Hoop h: hoops)
-                h.paint(g, robot, camera);
+//            for (Hoop h: hoops)
+//                h.paint(g, robot, camera);
         } finally {
             // Back to standard locations.
             g.translate(-getWidth() / 2, -getHeight() / 2);
