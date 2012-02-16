901342e9d5cba35b6a8ef4d2ca7e73ea9a09e6c2
Nate Williams <natewill@gmail.com>
2012-02-16 15:53:25 -0700

    - Fix silly bug where I'm using the wrong joystick for the shooter

--- FRC-2012/SimpleFrame.java
+++ FRC-2012/SimpleFrame.java
@@ -66,7 +66,7 @@ public class SimpleFrame extends SimpleRobot {
 
             // Read the throttle to determine the speed of the shooter
             // motor
-            shooterMotor.set(driveStick.getThrottle());
+            shooterMotor.set(shootStick.getThrottle());
         }
     }
 
