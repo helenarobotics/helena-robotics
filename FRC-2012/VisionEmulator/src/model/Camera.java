03bf3ea3dd121bb728561ca8852eb259ff382912
Nate Williams <nate@yogotech.com>
2012-02-18 14:15:17 -0700

    - Add camera details (pixels and FOV) to the model

--- FRC-2012/VisionEmulator/src/model/Camera.java
+++ FRC-2012/VisionEmulator/src/model/Camera.java
@@ -4,6 +4,23 @@ import java.util.Observable;
 
 // This class keeps track of the state of the camera on the robot
 public class Camera extends Observable {
+    // Global constants
+
+    // Field of view.  Since the camera has a 4:3 perspective, the
+    // vertical FOV is reduced by the inverse, or 3/4.
+    public static final double HORIZONTAL_FOV = 54.0;
+    public static final double VERTICAL_FOV = 54.0 * 3.0 / 4.0;
+
+    // Camera pixels
+    public static final double HORIZONTAL_PIXELS = 640;
+    public static final double VERTICAL_PIXELS = 480;
+
+    // We convert angles to pixels to view them on the screen.
+    public static final double HORIZONTAL_ANGLE_TO_PIXELS =
+        HORIZONTAL_PIXELS / HORIZONTAL_FOV;
+    public static final double VERTICAL_ANGLE_TO_PIXELS =
+        VERTICAL_PIXELS / VERTICAL_FOV;
+
     // Limit how far the camera can tilt
     private static final int MAX_ANGLE_AZIMUTH = 45;
     private static final int MIN_ANGLE_AZIMUTH = -45;
