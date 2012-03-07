257c0f404b8aa45ddd073d0ea9dd11fffa6b3e94
james arnold <jamesfarnold@gmail.com>
2012-03-06 21:45:01 -0700

    Not done, but getting there.  Still working the enclosing polygon issue..

--- FRC-2012/ImageProcessing/src/IU.java
+++ FRC-2012/ImageProcessing/src/IU.java
@@ -46,49 +46,38 @@ public class IU {
 		EdgeDetect edges = new EdgeDetect(image, 80);
 
 		RegionGrow RG = new RegionGrow(edges.thresholdedImage, 2, 200);
-		Region region = RG.grow(edges.thresholdedImage, 2, 200);
-		System.out.println(region);
-
-		System.out.println("Creating HT...");
-		HoughTransform h = new HoughTransform(image.getWidth(), image.getHeight(), thetas, nthetas);
-		h.addPoints(edges.edgesImage);      // This performs the actual transform; line above just constructor
-
-		// The following for debugging:
-
-		Vector<HoughLine> lines = h.getLines(10);  // XXX thresh was 15
-		BufferedImage himage = h.getHoughArrayImage();
-		ImageIO.write(himage, "jpg", new File("houghspace.jpg"));
-
-		// Create a color image from the hough results, and highlight the peaks in red
-		BufferedImage chimage = new BufferedImage(h.maxTheta, h.doubleHeight,  BufferedImage.TYPE_INT_RGB);
-		Graphics gh = chimage.getGraphics();  
-		gh.drawImage(himage, 0, 0, null);
-		gh.dispose(); 
+		System.out.println(RG);
 
 		// Also create color image from original input image.  We'll overlay HT lines and line segments.
 		BufferedImage cimage = new BufferedImage(image.getWidth(), image.getHeight(),  BufferedImage.TYPE_INT_RGB);
 		Graphics g = cimage.getGraphics();  
 		g.drawImage(image, 0, 0, null);
 
-		//draw bounding rectangle
-		if (region != null) {
+		//draw bounding rectangles
+		if (RG.regions != null) {
 		    Graphics2D g2 = cimage.createGraphics();
-		    Polygon p = new Polygon();
-		    Rectangle r = region.enclosingRectangle();
-		    p.addPoint(r.x, r.y);
-		    p.addPoint(r.x + r.width, r.y);
-		    p.addPoint(r.x + r.width, r.y + r.height);
-		    p.addPoint(r.x, r.y + r.height);
-		    g2.drawPolygon(p);
+		    for (int ir = 0; ir < RG.regions.size(); ir++) {
+			Region region = RG.regions.elementAt(ir);
+			Polygon p = new Polygon();
+			Rectangle r = region.enclosingRectangle();
+			p.addPoint(r.x, r.y);
+			p.addPoint(r.x + r.width, r.y);
+			p.addPoint(r.x + r.width, r.y + r.height);
+			p.addPoint(r.x, r.y + r.height);
+			g2.drawPolygon(p);
+			
+			Polygon p2 = region.enclosingPolygon(edges.thresholdedImage);
+			g2.drawPolygon(p2);
 
-		    int c = new Color(255, 255, 0).getRGB(); 
-		    for (int ir = 0; ir < region.size(); ir++) {
-			Point pt = region.elementAt(ir);
-			cimage.setRGB(pt.x, pt.y, c); 
+			int c = new Color(255, 255, 0).getRGB(); 
+			for (int iir = 0; iir < region.size(); iir++) {
+			    Point pt = region.elementAt(iir);
+			    cimage.setRGB(pt.x, pt.y, c); 
+			}
 		    }
 		}
 
-		Vector<Line2D.Double> totalSegments = new Vector<Line2D.Double>(30);
+		/*		Vector<Line2D.Double> totalSegments = new Vector<Line2D.Double>(30);
 
 		for (int j = 0; j < lines.size(); j++) { 
 		    HoughLine line = lines.elementAt(j); 
@@ -101,7 +90,7 @@ public class IU {
 
 		    totalSegments.addAll(segments);
 
-		    line.draw(cimage, Color.BLUE.getRGB());
+		    //		    line.draw(cimage, Color.BLUE.getRGB());
 		    
 		    System.out.print(segments.size() + " segments found " + j + ": ");
 		    for (int k = 0; k < segments.size(); k++) {
@@ -110,15 +99,16 @@ public class IU {
 					 (int)Math.sqrt(Math.pow(seg.x2 - seg.x1, 2) + Math.pow(seg.y2 - seg.y1, 2)) + 
 					 "> [{ " + seg.x1 + "," + seg.y1 + "}, {" + seg.x2 + "," + seg.y2 + "}] ");
 			//g.draw(edges.edgesImage, (int)seg.x1, (int)seg.y1, (int)seg.x2, (int)seg.y2);
-		    	HoughLine.drawsegment(edges.edgesImage, seg, Color.RED.getRGB());
-			HoughLine.drawsegment(cimage, seg, Color.RED.getRGB());
+		       	HoughLine.drawsegment(edges.edgesImage, seg, Color.RED.getRGB());
+			//HoughLine.drawsegment(cimage, seg, Color.RED.getRGB());
 		    }
 		    System.out.println();
+		}
+		// draw out line on image (for debugging and presentation)
 
-		    // draw out line on image (for debugging and presentation)
+		//		    System.out.println(j + ": " + line.peak + " [" + (int)((180/Math.PI)*line.theta) + ", " + line.r + "]");
+		//		    System.out.println("Neighborhood:");
 
-		    //		    System.out.println(j + ": " + line.peak + " [" + (int)((180/Math.PI)*line.theta) + ", " + line.r + "]");
-		    //		    System.out.println("Neighborhood:");
 		    for (int t = -2; t <=2; t++) {
 			for (int r = -2; r <= 2; r++)
 			    {
@@ -128,31 +118,33 @@ public class IU {
 				if (it >= h.maxTheta) it = it - h.maxTheta;
 				if (ir < 0) ir = h.doubleHeight + r;
 				if (ir >= h.doubleHeight) ir = ir - h.doubleHeight;
-				chimage.setRGB(it, ir, Color.RED.getRGB());
+			       chimage.setRGB(it, ir, Color.RED.getRGB());
 				//	System.out.print(h.houghArray[it][ir] + " ");
 			    }
 			//			System.out.println("");
 		    }
 		    //		   System.out.println("");
-		}
-		// Write out markup'd images
-		ImageIO.write(cimage, "jpg", new File("houghout.jpg"));
-		ImageIO.write(edges.edgesImage, "jpg", new File("houghoutedges.jpg"));
-		ImageIO.write(chimage, "jpg", new File("houghspaceoverlay.jpg"));
+		    */
+		    // Write out markup'd images
+		    ImageIO.write(cimage, "jpg", new File("overlay.jpg"));
+		    ImageIO.write(edges.edgesImage, "jpg", new File("houghoutedges.jpg"));
+		    ImageIO.write(edges.thresholdedImage, "jpg", new File("thresholdedImage.jpg"));
+		    //		    ImageIO.write(chimage, "jpg", new File("houghspaceoverlay.jpg"));
 		  
 
 		//	        Vector<Line2D.Double> segments = h.getLineSegments(edges.detected, 20);   // threshold = min # points required along a line of integration to declare detection.
 
-		System.out.println("Forming into boxes...");
-		Boxes boxes = new Boxes(totalSegments);
-		// System.out.println("Drawing boxes...");
-		boxes.draw(cimage);
-		//System.out.println("writing overlaid image...");
-		ImageIO.write(cimage, "jpg", new File("process" + fileNum + ".jpg"));
-		System.out.println(boxes.size() + " boxes found:");
-		//System.out.println("");
-		System.out.println(boxes);
-		//System.out.println("");
+		    /*		    System.out.println("Forming into boxes...");
+		    Boxes boxes = new Boxes(totalSegments);
+		    // System.out.println("Drawing boxes...");
+		    boxes.draw(cimage);
+		    //System.out.println("writing overlaid image...");
+		    ImageIO.write(cimage, "jpg", new File("process" + fileNum + ".jpg"));
+		    System.out.println(boxes.size() + " boxes found:");
+		    //System.out.println("");
+		    System.out.println(boxes);
+		    //System.out.println("");
+		    */
 		}
             } catch (IOException e) {
                 e.printStackTrace();
@@ -174,4 +166,7 @@ public class IU {
 	}
 	return raw;
     }
+
+
+    // End of class..
 }
--- FRC-2012/ImageProcessing/src/Region.java
+++ FRC-2012/ImageProcessing/src/Region.java
@@ -1,5 +1,8 @@
 import java.util.Vector;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.awt.geom.*;
+import java.awt.Polygon;
 
 public class Region {
 
@@ -36,13 +39,207 @@ public class Region {
 
 	return new Rectangle(minX, minY, (maxX - minX), (maxY - minY));
     }
+
 	    
+    public Polygon enclosingPolygon(BufferedImage img) {
+	Rectangle r = enclosingRectangle();
+	dataPoint [] dp = new dataPoint [Math.max(r.width, r.height) + 1];
+
+	// Calculate best line fit to left edge (using thresholded image data)
+	int count = 0;
+	for (int y = r.y; y < r.y + r.height; y++) {
+	    for (int x = r.x; x < r.x + r.width; x++) {
+		if ((img.getRGB(x, y) & 0x000000ff) > 0) {
+		    dp[count] = new dataPoint(x, y);
+		    count++;
+		    break;
+		}
+	    }
+	}
+	double resultsLeft [] = leastSquares(dp, count, count/2);
+
+	// Calculate best line fit to right edge (using thresholded image data)
+	count = 0;
+	for (int y = r.y; y < r.y + r.height; y++) {
+	    for (int x = r.x + r.width; x >=  r.x; x--) {
+		if ((img.getRGB(x, y) & 0x000000ff) > 0) {
+		    dp[count] = new dataPoint(x, y);
+		    count++;
+		    break;
+		}
+	    }
+	}
+	double resultsRight [] = leastSquares(dp, count, count/2);
+
+	// Calculate best line fit to top edge (using thresholded image data)
+	count = 0;
+	for (int x = r.x; x < r.x + r.width; x++) {
+	    for (int y = r.y; y < r.y + r.height; y++) {
+		if ((img.getRGB(x, y) & 0x000000ff) > 0) {
+		    dp[count] = new dataPoint(x, y);
+		    count++;
+		    break;
+		}
+	    }
+	}
+	double resultsTop [] = leastSquares(dp, count, count/2);
+
+	// Calculate best line fit to bottom edge (using thresholded image data)
+	count = 0;
+	for (int x = r.x; x < r.x + r.width; x++) {
+	    int y;
+	    for (y = r.y + r.height; y > r.y; y--) {
+		if ((img.getRGB(x, y) & 0x000000ff) > 0) {
+		    dp[count] = new dataPoint(x, y);
+		    count++;
+		    break;
+		}
+	    }
+	}
+	double resultsBottom [] = leastSquares(dp, count, count/2);
+
+
+	// Now find the four points of intersection
+	Point2D.Double leftTop = intersectingPoint(resultsTop[0], resultsTop[1], resultsLeft[0], resultsLeft[1]);
+	Point2D.Double rightTop = intersectingPoint(resultsTop[0], resultsTop[1], resultsRight[0], resultsRight[1]);
+	Point2D.Double leftBottom = intersectingPoint(resultsBottom[0], resultsBottom[1], resultsLeft[0], resultsLeft[1]);
+	Point2D.Double rightBottom = intersectingPoint(resultsBottom[0], resultsBottom[1], resultsRight[0], resultsRight[1]);
+
+	System.out.println("Top left corner at     " + (int)leftTop.x + ", " +  (int)leftTop.y);
+	System.out.println("Top right corner at    " + (int)rightTop.x + ", " +  (int)rightTop.y);
+	System.out.println("Bottom right corner at " + (int)rightBottom.x + ", " +  (int)rightBottom.y);
+	System.out.println("Bottom left corner at " +  (int)leftBottom.x + ", " +  (int)leftBottom.y);
+
+	Polygon p = new Polygon();
+
+	p.addPoint((int) leftTop.x, (int) leftTop.y);
+	p.addPoint((int) rightTop.x, (int) rightTop.y);
+	p.addPoint((int) rightBottom.x, (int) rightBottom.y);
+	p.addPoint((int) leftBottom.x, (int) leftBottom.y);
+
+	return p;
+    }
+
+    static Point2D.Double intersectingPoint(double m1, double b1, double m2, double b2) {
+	double x = (b2 - b1) / (m1 - m2);   // x at point of intersection (if they intersect!)
+	return (new Point2D.Double(x, x * m1 + b1));
+    }
+
+    // Simple least squares linear fit algorithm.  Returns a 2-long array of doubles, the first (slot [0]) of which 
+    // represents m, 2nd b, in the familiar line equation y = mx + b
+
+    private double [] leastSquares(dataPoint dp [], int n, int keep) {
+	leastSquares(dp, n);
+	quickSort(dp, 0, n-1);
+	/*	System.out.println("Errors after sorting: ");
+	for (int i = 0; i < n; i++) {
+	    System.out.print(" {" + (int)dp[i].x + "," + (int)dp[i].y + "}=" + dp[i].err);
+	}
+	*/
+	return (leastSquares(dp, keep));
+    }
+
+    private double [] leastSquares(dataPoint dp [], int n) {
+
+	double sumx = 0.0, sumx2 = 0.0, sumy = 0;
+
+	for (int i = 0; i < n; i++) {
+	    sumx  += dp[i].x;
+	    sumx2 += dp[i].x * dp[i].x;
+	    sumy  += dp[i].y;
+	}
+
+	double xbar = sumx / n;
+	double ybar = sumy / n;
+
+	// second pass: compute summary statistics
+	double xxbar = 0.0, yybar = 0.0, xybar = 0.0;
+	for (int i = 0; i < n; i++) {
+	    xxbar += (dp[i].x - xbar) * (dp[i].x - xbar);
+	    yybar += (dp[i].y - ybar) * (dp[i].y - ybar);
+	    xybar += (dp[i].x - xbar) * (dp[i].y - ybar);
+	}
+	double beta1 = xybar / xxbar;
+	double beta0 = ybar - beta1 * xbar;
+
+	// Calculate and store the errors
+	double err2 = 0;
+	for (int i = 0; i < n; i++) {
+	    double err = dp[i].y - beta1 * dp[i].x - beta0; 
+	    dp[i].err = err * err;
+	    err2 += dp[i].err;
+	}
+
+	// print results
+	System.out.println("y   = " + beta1 + " * x + " + beta0 + " std err " + Math.sqrt(err2/(double)n));
+
+	double result [] = new double [2];
+	result[0] = beta1;
+	result[1] = beta0;
+	return (result);
+    }
+	
 
     public String toString() {
 	Rectangle e = enclosingRectangle();
 	int xc = e.x + e.width/2;
 	int yc = e.y + e.height/2;
 	return "Centers on {" + xc + ", " + yc + "} width = " + e.width + ", height = " + e.height;
+    }
+
+    private class dataPoint {
+	double x, y, err;
+
+	dataPoint(double _x, double _y, double _err) {
+	    x = _x;
+	    y = _y;
+	    err = _err;
+	}
+
+	dataPoint(double _x, double _y) {
+	    x = _x;
+	    y = _y;
+	    err = 0.0;
+	}
+
+	dataPoint() {
+	    x = y = err = 0;
+	}
+    }
+	
+
+    //Quick Sort code for JAVA, by Yash Gupta
+    // (Rebuilt by Arnold to sort class type dataPoint
+
+private int partition(dataPoint arr[], int left, int right){
+	int i = left, j = right;
+	dataPoint tmp;
+	double pivot = arr[(left + right) / 2].err;
+	while (i <= j)
+	    {
+		while (arr[i].err < pivot)
+		    i++;
+		while (arr[j].err > pivot)
+		    j--;
+		if (i <= j)
+		    {
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+			i++;
+			j--;
+		    }
+	    };
+	return i;
+    }
+
+private void quickSort(dataPoint arr[], int left, int right)
+    {
+	int index = partition(arr, left, right);
+	if (left < index - 1)
+            quickSort(arr, left, index - 1);
+	if (index < right)
+            quickSort(arr, index, right);
     }
 }
 
