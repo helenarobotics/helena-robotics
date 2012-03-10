3373ec5e1107366242a260a609dc129b6dacd764
Nate Williams <nate@yogotech.com>
2012-03-10 12:23:16 -0700

    - Move this into the Robot directory

---
+++ FRC-2012/Robot/SimpleFrame.java
@@ -0,0 +1,94 @@
+package robotics.helena;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Victor;
+
+public class SimpleFrame extends SimpleRobot {
+    // Drivetrain
+    private Victor leftMotor;
+    private Victor rightMotor;
+    private Solenoid shifter;
+
+    // Shooter
+    private Victor shooterMotor;
+
+    // Accelerometer
+    DIMUAccel accel;
+
+    SimpleFrame() {
+        System.out.println("Starting SimpleFrame");
+        leftMotor = new Victor(1);
+        rightMotor = new Victor(2);
+        shooterMotor = new Victor(3);
+        disabled();
+
+        // Setup the transmission shifter.
+        shifter = new Solenoid(1);
+        shifter.set(true);
+
+        // Initialize the accelerometer
+        accel = new DIMUAccel(1);
+    }
+
+    protected void disabled() {
+        leftMotor.set(0);
+        rightMotor.set(0);
+        shooterMotor.set(0);
+    }
+
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public void autonomous() {
+        System.out.println("No Autonomous mode code");
+    }
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        System.out.println("Starting operatorControl");
+
+        Joystick driveStick = new Joystick(1);
+        Joystick shootStick = new Joystick(2);
+        RobotDrive drive = new RobotDrive(leftMotor, rightMotor);
+
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, false);
+
+        // Turn off drive safety which causes the motors to stop running
+        // if they don't get a command within a couple of ms.
+        drive.setSafetyEnabled(false);
+
+        while (isOperatorControl() && isEnabled()) {
+            // Ignore the dead-spot near the center of the drive joystick
+            if (Math.abs(driveStick.getMagnitude()) > 0.05) {
+                // Add greater sensitivity at lower magnitudes
+                drive.arcadeDrive(driveStick, true);
+            }
+            joystickShifter(driveStick);
+
+            // Read the throttle to determine the speed of the shooter
+            // motor.  The throttle goes from -1.0 <-> 1.0, but we only
+            // want the values 0 -> 1.0.  (We change the sign since
+            // the motor is hooked up backward).
+            shooterMotor.set(-(shootStick.getThrottle() + 1.0)/ 2.0);
+
+            // Print out the Z-Axis of the accelerometer.
+            System.out.println(accel.getCurrentAccel(DIMUAccel.Z_AXIS));
+        }
+    }
+
+    private static final int TRIGGER_BTN = 1;
+    private boolean wasPressed = false;
+    private void joystickShifter(Joystick stick) {
+        // Toggle the shifter when the shifter button is pressed
+        boolean nowPressed = stick.getRawButton(TRIGGER_BTN);
+        if (nowPressed && !wasPressed) {
+            shifter.set(!shifter.get());
+        }
+        wasPressed = nowPressed;
+    }
+}
