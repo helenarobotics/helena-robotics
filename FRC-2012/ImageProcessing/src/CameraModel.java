35a6eeec5ec998a3bfea7a09f0681c06edaa6d64
james arnold <jamesfarnold@gmail.com>
2012-03-19 21:17:40 -0600

    Split camera model from field geometry model, cleaned up code

---
+++ FRC-2012/ImageProcessing/src/CameraModel.java
@@ -0,0 +1,17 @@
+public class CameraModel {    // We might move the camera model to its own class:
+    public static double cameraHeight = 37.0;   // inches (an estimate for now)
+    public static double cameraTilt = 11.0 * Math.PI/180.0;     // estimated from data set of March 2; we need to nail this down
+    public static final double cameraHorizontalView = 54.0 * Math.PI/180.0;
+    public static final double cameraVerticalView = 40.5 * Math.PI/180.0;
+    public static int cameraXpixels = 320;        // this needs to come from actual image size
+    public static int cameraYpixels = 240;
+
+    public CameraModel() {
+    }
+
+    public String toString() {
+	return ("Camera " + cameraXpixels + " (" +  cameraHorizontalView + " deg) x" + cameraYpixels + " (" + cameraVerticalView + " deg), tilt " + cameraTilt + ", ht " + cameraHeight + "inches");
+    }
+
+}
+
--- FRC-2012/ImageProcessing/src/FieldGeometry.java
+++ FRC-2012/ImageProcessing/src/FieldGeometry.java
@@ -6,20 +6,14 @@ import javax.vecmath.Point3d;
 //     public enum HoopLocation { unknown, left, top, right, bottom };
 
 public class FieldGeometry {
-    // rectangles beginning with top left corner of each hoop, moving clockwise.
+    // Known locations of the outside corners of the four hoops:
     public static Rectangle2D.Double leftHoop = new Rectangle2D.Double(-39.375, 81.0, 24.0, 18.0);
     public static Rectangle2D.Double rightHoop = new Rectangle2D.Double(39.375, 81.0, 24.0, 18.0);
     public static Rectangle2D.Double topHoop = new Rectangle2D.Double(-12.0, 118.0, 24.0, 18.0);
     public static Rectangle2D.Double bottomHoop = new Rectangle2D.Double(-12.0, 48.0, 24.0, 18.0);
 
-    public static double cameraHeight = 37.0;   // inches (an estimate for now)
-    public static double cameraTilt = 11.0 * Math.PI/180.0;     // estimated from data set of March 2; we need to nail this down
-    public static final double cameraHorizontalView = 54.0 * Math.PI/180.0;
-    public static final double cameraVerticalView = 40.5 * Math.PI/180.0;
-    public static int cameraXpixels = 320;
-    public static int cameraYpixels = 240;
-
     // leftTop, rightTop, leftBottom, rightBottom;
+
     static void  estimateRange(Region hoop) {
 	
 	double top = 0.0, bot = 0.0, left = 0.0, right = 0.0;
@@ -58,57 +52,23 @@ public class FieldGeometry {
 
 	// Estimate range to 4 corners, as possible
 
-	double x, y, h;
-
 	if (hoop.leftTop != null) {
-	    h = top  - cameraHeight;
-	    y = hoop.leftTop.y;
-	    x = hoop.leftTop.x;
-	    HoopEstimate hr = new HoopEstimate();
-	    hr.elevation = (cameraYpixels/2 - y) * cameraVerticalView / cameraYpixels + cameraTilt;
-	    hr.azimuth = x * cameraHorizontalView / cameraXpixels;
-	    hr.rangePoint = new Point3d(left, 0.0, top);      // We need to confirm consistent use of 3D axis: x, y, z
-	    hr.range = Math.abs(h / Math.sin(hr.elevation));   // this might even be correct.
-	    hr.error = Math.abs(h * Math.cos(hr.elevation) * cameraVerticalView / cameraYpixels / (Math.sin(hr.elevation) * Math.sin(hr.elevation)));
-	    hoop.estimates.add(hr);                            // Save for later
+	    HoopEstimate hr = new HoopEstimate(hoop.leftTop.x, hoop.leftTop.y, left, top);
+	    hoop.estimates.add(hr);                            // Save for later.  We'll combine the various range estimates to calc robot position etc.
 	}
 
 	if (hoop.rightTop != null) {
-	    y = hoop.rightTop.y;
-	    x = hoop.rightTop.x;
-	    h = top - cameraHeight;
-	    HoopEstimate hr = new HoopEstimate();
-	    hr.elevation = (cameraYpixels/2 - y) * cameraVerticalView / cameraYpixels + cameraTilt;
-	    hr.azimuth = x * cameraHorizontalView / cameraXpixels;
-	    hr.rangePoint = new Point3d(right, 0.0, top);      // We need to confirm consistent use of 3D axis: x, y, z
-	    hr.range = Math.abs(h / Math.sin(hr.elevation));   // this might even be correct.
-	    hr.error = Math.abs(h * Math.cos(hr.elevation) * cameraVerticalView / cameraYpixels / (Math.sin(hr.elevation) * Math.sin(hr.elevation)));
+	    HoopEstimate hr = new HoopEstimate(hoop.rightTop.x, hoop.rightTop.y, right, top);
 	    hoop.estimates.add(hr);                            // Save for later
 	}
-	// Now estimate range to bottom edge, if possible
+
 	if (hoop.leftBottom != null) {
-	    y = hoop.leftBottom.y;
-	    x = hoop.leftBottom.x;
-	    h = bot - cameraHeight;
-	    HoopEstimate hr = new HoopEstimate();
-	    hr.elevation = (cameraYpixels/2 - y) * cameraVerticalView / cameraYpixels + cameraTilt;
-	    hr.azimuth = x * cameraHorizontalView / cameraXpixels;
-	    hr.rangePoint = new Point3d(left, 0.0, bot);      // We need to confirm consistent use of 3D axis: x, y, z
-	    hr.range = Math.abs(h / Math.sin(hr.elevation));   // this might even be correct.
-	    hr.error = Math.abs(h * Math.cos(hr.elevation) * cameraVerticalView / cameraYpixels / (Math.sin(hr.elevation) * Math.sin(hr.elevation)));
+	    HoopEstimate hr = new HoopEstimate(hoop.leftBottom.x, hoop.leftBottom.y, left, bot);
 	    hoop.estimates.add(hr);                            // Save for later
 	}
-	// Now estimate range to bottom right corner, if possible
+
 	if (hoop.rightBottom != null) {
-	    y = hoop.rightBottom.y;
-	    x = hoop.rightBottom.x;
-	    h = bot - cameraHeight;
-	    HoopEstimate hr = new HoopEstimate();
-	    hr.elevation = (cameraYpixels/2 - y) * cameraVerticalView / cameraYpixels + cameraTilt;
-	    hr.azimuth = x * cameraHorizontalView / cameraXpixels;
-	    hr.rangePoint = new Point3d(right, 0.0, bot);      // We need to confirm consistent use of 3D axis: x, y, z
-	    hr.range = Math.abs(h / Math.sin(hr.elevation));   // this might even be correct.
-	    hr.error = Math.abs(h * Math.cos(hr.elevation) * cameraVerticalView / cameraYpixels / (Math.sin(hr.elevation) * Math.sin(hr.elevation)));
+	    HoopEstimate hr = new HoopEstimate(hoop.rightBottom.x, hoop.rightBottom.y, right, bot);
 	    hoop.estimates.add(hr);                            // Save for later
 	}
     }
