04f84a50acaa57202009337e9ce0e1e90b98395d
Nate Williams <nate@yogotech.com>
2012-04-01 16:22:16 -0600

    - Update location for Bridge Arm and Solenoid

--- FRC-2012/ShooterRobot/Configuration.java
+++ FRC-2012/ShooterRobot/Configuration.java
@@ -13,7 +13,7 @@ public class Configuration {
     public static final int VICTOR_LOWER_SHOOTER = 4;
     public static final int JAGUAR_SHOOTER_ROTATION = 6;
     public static final int SERVO_SHOOTER_FEEDER = 8;
-    public static final int BRIDGE_ARM_MOTOR = 10;
+    public static final int VICTOR_BRIDGE_ARM = 10;
 
     // I2C bus
     public static final int I2C_ARDUINO = 1;
@@ -21,5 +21,6 @@ public class Configuration {
     // Analog channels
     public static final int GYRO_ANALOG_CHANNEL = 1;
 
-    // XXX - Solenoid slot?
+    // Solenoid slot?
+    public static final int SOLENOID_CHANNEL = 8;
 }
