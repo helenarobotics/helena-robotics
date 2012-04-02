0b7ad16f45924bad8bc3d4cc6a6cae7c94497c83
Nate Williams <nate@yogotech.com>
2012-04-01 20:22:32 -0600

    - Instead of busy-waiting for new images, have the consumer block waiting for   a new image.

--- FRC-2012/Widgets/CameraWidget/ImageQueue.java
+++ FRC-2012/Widgets/CameraWidget/ImageQueue.java
@@ -1,20 +1,34 @@
 import java.awt.image.BufferedImage;
 
 public class ImageQueue {
-    BufferedImage image;
+    private Object syncObject = new Object();
+    private BufferedImage image;
 
     public ImageQueue() {
         image = null;
     }
 
-    public synchronized BufferedImage get() {
-        BufferedImage tmp = image;
-        image = null;     // once consumed, remove it from our 1 - slot queue
-
+    public BufferedImage get() {
+        BufferedImage tmp;
+        synchronized (syncObject) {
+            // Block until we have a new image!
+            while (image == null) {
+                try {
+                    syncObject.wait();
+                } catch (InterruptedException ignored) {
+                }
+            }
+            // once consumed, remove it from our 1 - slot queue
+            tmp = image;
+            image = null;
+        }
         return tmp;
     }
 
-    public synchronized void put(BufferedImage _image) {
-        image = _image;
+    public void put(BufferedImage _image) {
+        synchronized (syncObject) {
+            image = _image;
+            syncObject.notifyAll();
+        }
     }
 }
