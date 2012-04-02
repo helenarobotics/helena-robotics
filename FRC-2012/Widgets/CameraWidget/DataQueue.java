886ffd2571c48be042982d014bfab1ecb63319ca
Nate Williams <nate@yogotech.com>
2012-04-01 20:29:32 -0600

    - Make sure the internal variable is not accessed by making it private   to ensure we are thread-safe.

--- FRC-2012/Widgets/CameraWidget/DataQueue.java
+++ FRC-2012/Widgets/CameraWidget/DataQueue.java
@@ -1,11 +1,13 @@
 public class DataQueue {
-    /* 'hoopset' holds all results, including outlines of hoops (via
+    /*
+     * 'hoopset' holds all results, including outlines of hoops (via
      * regions vector contained within), and estimated 'robotPosition'
-     * (as Point3d, where x = horiz line along back wall of court, y =
-     * vertical height (always set to current camera height), z =
-     * distance from wall.
+     * as Point3d, where:
+     * x = horiz line along back wall of court
+     * y = vertical height (always set to current camera height)
+     * z = * distance from wall
      */
-    public ImageResults hoopset;
+    private ImageResults hoopset;
 
     public DataQueue() {
         hoopset = null;
