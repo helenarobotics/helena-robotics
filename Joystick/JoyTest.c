2d93338e68184ebdb57f7aedb9444e6a55fe8e46
Nate Williams <nate@yogotech.com>
2012-10-09 21:13:55 -0600

    - Slight modifications to ensure that we don't leave character fragments   when the numbers get larger - Also check at least one button.

--- Joystick/JoyTest.c
+++ Joystick/JoyTest.c
@@ -9,12 +9,14 @@ task main()
 
         int x = joystick.joy1_x1;
         int y = joystick.joy1_y1;
-        nxtDisplayString(0, "X <%d>", x);
-        nxtDisplayString(1, "Y <%d>", y);
+        nxtDisplayString(0, "X <%d>     ", x);
+        nxtDisplayString(1, "Y <%d>     ", y);
 
         x = joystick.joy1_x2;
         y = joystick.joy1_y2;
-        nxtDisplayString(2, "X <%d>", x);
-        nxtDisplayString(3, "Y <%d>", y);
+        nxtDisplayString(2, "X <%d>    ", x);
+        nxtDisplayString(3, "Y <%d>    ", y);
+
+        nxtDisplayString(5, "%s", (joy1Btn(5) ? "Y" : "N"));
     }
 }
