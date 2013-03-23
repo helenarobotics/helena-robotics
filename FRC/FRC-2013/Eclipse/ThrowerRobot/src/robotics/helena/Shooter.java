7abbbf05df5a5b78d5d61357926b6dc594ff310d
Nate Williams <nate@yogotech.com>
2013-03-23 13:21:25 -0600

    - Move all of the frisbee joystick controls into the Shooter and out   of the main 'Thrower' class.

--- FRC/FRC-2013/Eclipse/ThrowerRobot/src/robotics/helena/Shooter.java
+++ FRC/FRC-2013/Eclipse/ThrowerRobot/src/robotics/helena/Shooter.java
@@ -1,11 +1,18 @@
 package robotics.helena;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Shooter {
     public static final int MAX_RPM = 225;
     private static final int RPM_TOLERANCE = 5;
+
+    private static final double JOY_Y_SHOOTER_ADJUSTMENT_PERCENT = 20.0 / 100;
+
+    private final Loader loader;
     private final PIDVictor pidShooter;
+
+    private boolean shooterOn = false;
 
     public Shooter() {
         pidShooter = new PIDVictor(
@@ -15,11 +22,60 @@ public class Shooter {
                 1.0, 0.0, 0.0,
                 RPM_TOLERANCE, MAX_RPM);
         pidShooter.setPower(0);
+        loader = new Loader();
     }
 
     public void setPower(double power) {
-        if (pidShooter.getPower() != power) {
+        if (pidShooter.getPower() != power)
             pidShooter.setPower(power);
+    }
+    
+    public void shootFrisbee() {
+        loader.loadNext();
+    }
+
+    public void control(Joystick joy) {
+        // Is the shooter turned on?
+        checkShooterState(joy);
+
+        if (shooterOn) {
+            // Motor control.  Coarse settings are done via the joystick throttle,
+            // and finer settings (+- 20%) are done via the y-axis of the joystick.
+            double motorPower = (joy.getThrottle() - 1.0) / 2.0;
+            motorPower += (joy.getY() * JOY_Y_SHOOTER_ADJUSTMENT_PERCENT);
+            DashboardComm.powerThrower = motorPower;
+            setPower(motorPower);
+
+            // Shoot frisbee if trigger pressed - Note, this is only allowed
+            // if the shooter is on.
+            if (joystickTrigger(joy))
+                shootFrisbee();
         }
+    }
+
+    // Shooter button state
+    private boolean fireWasPressed = false;
+    private boolean shooterEnabledWasPressed = false;
+
+    private boolean joystickTrigger(Joystick joy) {
+        boolean btnPressed = false;
+
+        // Fire a frisbee when the button is pressed
+        boolean nowPressed = joy.getRawButton(Configuration.FIRE_BUTTON);
+        if (nowPressed && !fireWasPressed) {
+            btnPressed = true;
+        }
+        fireWasPressed = nowPressed;
+
+        return btnPressed;
+    }
+
+    private void checkShooterState(Joystick joy) {
+        // Check the button state
+        boolean nowPressed = joy.getRawButton(Configuration.SHOOTER_ENABLE_BUTTON);
+        if (nowPressed && !shooterEnabledWasPressed) {
+            shooterOn = !shooterOn;
+        }
+        shooterEnabledWasPressed = nowPressed;
     }
 }
--- FRC/FRC-2013/Eclipse/ThrowerRobot/src/robotics/helena/Thrower.java
+++ FRC/FRC-2013/Eclipse/ThrowerRobot/src/robotics/helena/Thrower.java
@@ -4,25 +4,18 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SimpleRobot;
 
 public class Thrower extends SimpleRobot {
-    private static final double FRIZ_JOY_Y_SHOOTER_ADJUSTMENT_PERCENT = 20.0 / 100;
-
-    private final Loader loader;
     private final Shooter shooter;
-
 //    private final Grabber grabber;
 //    private final Lift lift;
 
     public Thrower() {
         shooter = new Shooter();
-        loader = new Loader();
 //        grabber = new Grabber();
 //        lift = new Lift();
     }
 
     public void autonomous() {
     }
-
-    boolean shooterOn = false;
 
     public void operatorControl() {
         //Setup drive motors
@@ -36,55 +29,9 @@ public class Thrower extends SimpleRobot {
 //            grabber.move(frizJoy);
 //            lift.control(frizJoy);
 
-            //
-            // Check the joystick buttons
-            //
-
-            // Is the shooter turned on?
-            checkShooterState(driveJoy);
-
-            if (shooterOn) {
-                // Frisbee motor.  Coarse settings are done via the throttle, and
-                // finer settings (+- 20%) are done via the y-axis of the joystick.
-                double motorPower = (frizJoy.getThrottle() - 1.0) / 2.0;
-                motorPower += (frizJoy.getY() * FRIZ_JOY_Y_SHOOTER_ADJUSTMENT_PERCENT);
-                DashboardComm.powerThrower = motorPower;
-                shooter.setPower(motorPower);
-
-                // Shoots frisbee if trigger pressed - Note, this is only allowed
-                // if the shooter is on.
-                if (joystickTrigger(frizJoy)) {
-                    System.out.println("Load Me");
-                    loader.loadNext();
-                }
-            }
+            // Run the shooter!
+            shooter.control(frizJoy);
         }
-    }
-
-    // Shooter button state
-    private boolean fireWasPressed = false;
-    private boolean shooterEnabledWasPressed = false;
-
-    private boolean joystickTrigger(Joystick joy) {
-        boolean btnPressed = false;
-
-        // Fire a frisbee when the button is pressed
-        boolean nowPressed = joy.getRawButton(Configuration.FIRE_BUTTON);
-        if (nowPressed && !fireWasPressed) {
-            btnPressed = true;
-        }
-        fireWasPressed = nowPressed;
-
-        return btnPressed;
-    }
-
-    private void checkShooterState(Joystick joy) {
-        // Check the button state
-        boolean nowPressed = joy.getRawButton(Configuration.SHOOTER_ENABLE_BUTTON);
-        if (nowPressed && !shooterEnabledWasPressed) {
-            shooterOn = !shooterOn;
-        }
-        shooterEnabledWasPressed = nowPressed;
     }
 
     /**
