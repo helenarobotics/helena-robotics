4b41a0fdae3b5d44ff813d33751a26a93031303b
Nate Williams <nate@yogotech.com>
2013-10-14 08:22:10 -0600

    - Code we created during our software session

---
+++ FTC/2013-14/SoftwareSession/AutoTemplate.c
@@ -0,0 +1,120 @@
+
+// Forward declarations
+void turnRight(int degrees);
+void turnLeft(int degrees);
+
+// These are robot specific
+const int ONE_CM_TICKS = 30;
+const int TICKS_PER_DEGREE = 8 * 1080 / 360;
+
+// If these are modified, the above may need to be altered slightly
+const int STRAIGHT_MOTOR_POWER = 50;
+const int TURN_MOTOR_POWER = 50;
+
+const int MOVE_WAIT_TIME = 200;
+
+void initializeRobot()
+{
+    motor[motorA] = 0;
+    motor[motorB] = 0;
+}
+
+void moveStraight(int distanceInCm)
+{
+    // Move in one foot increments
+    int power = STRAIGHT_MOTOR_POWER;
+    if (distanceInCm < 0)
+        power = -power;
+
+    nMotorEncoder[motorA] = 0;
+    nMotorEncoder[motorB] = 0;
+    motor[motorA] = power;
+    motor[motorB] = power;
+    int ticks = ONE_CM_TICKS * abs(distanceInCm);
+    while (abs(nMotorEncoder[motorA]) < ticks ||
+           abs(nMotorEncoder[motorB]) < ticks)
+    {
+        if (abs(nMotorEncoder[motorA]) >= ticks)
+            motor[motorA] = 0;
+        if (abs(nMotorEncoder[motorB]) >= ticks)
+            motor[motorB] = 0;
+    }
+    motor[motorA] = 0;
+    motor[motorB] = 0;
+
+    // Wait for the robot to quit moving
+    wait1Msec(MOVE_WAIT_TIME);
+}
+
+void turnLeft(int degrees)
+{
+    // If they give a negative value, we're turning the other way
+    if (degrees < 0)
+    {
+        turnRight(abs(degrees));
+        return;
+    }
+    nMotorEncoder[motorA] = 0;
+    nMotorEncoder[motorB] = 0;
+    motor[motorA] = -TURN_MOTOR_POWER;
+    motor[motorB] = TURN_MOTOR_POWER;
+    int ticks = TICKS_PER_DEGREE * degrees;
+    while (abs(nMotorEncoder[motorA]) < ticks ||
+           abs(nMotorEncoder[motorB]) < ticks)
+    {
+        if (abs(nMotorEncoder[motorA]) >= ticks)
+            motor[motorA] = 0;
+        if (abs(nMotorEncoder[motorB]) >= ticks)
+            motor[motorB] = 0;
+    }
+    motor[motorA] = 0;
+    motor[motorB] = 0;
+
+    // Wait for the robot to stop moving
+    wait1Msec(MOVE_WAIT_TIME);
+}
+
+void turnRight(int degrees)
+{
+    // If they give a negative value, we're turning the other way
+    if (degrees < 0)
+    {
+        turnLeft(abs(degrees));
+        return;
+    }
+    nMotorEncoder[motorA] = 0;
+    nMotorEncoder[motorB] = 0;
+    motor[motorA] = TURN_MOTOR_POWER;
+    motor[motorB] = -TURN_MOTOR_POWER;
+    int ticks = TICKS_PER_DEGREE * degrees;
+    while (abs(nMotorEncoder[motorA]) < ticks ||
+           abs(nMotorEncoder[motorB]) < ticks)
+    {
+        if (abs(nMotorEncoder[motorA]) >= ticks)
+            motor[motorA] = 0;
+        if (abs(nMotorEncoder[motorB]) >= ticks)
+            motor[motorB] = 0;
+    }
+    motor[motorA] = 0;
+    motor[motorB] = 0;
+
+    // Wait for the robot to stop moving
+    wait1Msec(MOVE_WAIT_TIME);
+}
+
+task main()
+{
+    initializeRobot();
+
+    // Place the cube in the scoring zone and move to the ramp
+    moveStraight(45);
+    turnLeft(45);
+    moveStraight(1);
+    moveStraight(-1);
+    turnLeft(90);
+    moveStraight(3);
+    turnRight(90);
+    moveStraight(2);
+    turnRight(90);
+    moveStraight(2);
+}
