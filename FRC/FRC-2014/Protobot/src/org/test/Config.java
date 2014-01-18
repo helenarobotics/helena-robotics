03676d559959f7d579209d0e0cfe508243d24a07
jmiller <jmiller@CH-RBT02-FW6BQ7.hsd1.org>
2014-01-18 15:23:15 -0700

    Fist prototype robot program; Runs drive motors and a solenoid.

---
+++ FRC/FRC-2014/Protobot/src/org/test/Config.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.test;
+
+/**
+ *
+ * @author jmiller
+ */
+public class Config {
+    //Victor Channels
+    public static final int FRONT_LEFT_VICTOR =  1;
+    public static final int FRONT_RIGHT_VICTOR = 2;
+    public static final int BACK_LEFT_VICTOR =   3;
+    public static final int BACK_RIGHT_VICTOR =  4;
+   
+    //Solenoid(s) Channel
+    public static final int KICKER_SOLENOID = 1;
+    
+    //DRIVE TYPES
+    public static final int ARCADE =   1;
+    public static final int MECHANUM = 2;
+    
+    //Drive Type Option
+    public static final int DRIVE_TYPE = MECHANUM;
+    
+    //Joystick Channels
+    public static final int DRIVE_JOYSTICK = 1;
+    
+    //Joystick Buttons
+    public static final int KICKER_BUTTON = 1;
+}
